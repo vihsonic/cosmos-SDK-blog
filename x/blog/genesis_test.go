fc825a653a9d9fc0739d8aa1ebcb5803269e418e
black <black02430horse@gmail.com>
2023-11-01 08:43:20 -0500

    feature/boiler-plate

---
+++ x/blog/genesis_test.go
@@ -0,0 +1,29 @@
+package blog_test
+
+import (
+	"testing"
+
+	keepertest "blog/testutil/keeper"
+	"blog/testutil/nullify"
+	"blog/x/blog"
+	"blog/x/blog/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGenesis(t *testing.T) {
+	genesisState := types.GenesisState{
+		Params: types.DefaultParams(),
+
+		// this line is used by starport scaffolding # genesis/test/state
+	}
+
+	k, ctx := keepertest.BlogKeeper(t)
+	blog.InitGenesis(ctx, *k, genesisState)
+	got := blog.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+
+	// this line is used by starport scaffolding # genesis/test/assert
+}
