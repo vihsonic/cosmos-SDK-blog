bc0b64b6c3de70352f7280a33241243b09c3d00a
black <black02430horse@gmail.com>
2023-11-01 20:09:26 -0500

    feat: delete-post

--- x/blog/keeper/msg_server_delete_post.go
+++ x/blog/keeper/msg_server_delete_post.go
@@ -1,10 +1,12 @@
 package keeper
 
 import (
+	"fmt"
 	"context"
 
 	"blog/x/blog/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) DeletePost(goCtx context.Context, msg *types.MsgDeletePost) (*types.MsgDeletePostResponse, error) {
@@ -12,6 +14,13 @@ func (k msgServer) DeletePost(goCtx context.Context, msg *types.MsgDeletePost) (
 
 	// TODO: Handling the message
 	_ = ctx
-
+	val, found := k.GetPost(ctx, msg.Id)
+	if !found {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrKeyNotFound, fmt.Sprintf("key %d doesn't exist", msg.Id))
+	}
+	if msg.Creator != val.Creator {
+		return nil, sdkerrors.Wrap(sdkerrors.ErrUnauthorized, "incorrect owner")
+	}
+	k.RemovePost(ctx, msg.Id)
 	return &types.MsgDeletePostResponse{}, nil
 }
